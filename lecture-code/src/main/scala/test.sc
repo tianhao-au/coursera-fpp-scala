c223a014a39287913a4561c11c5004a9f5c7edaf
Tianhao Li <ysihaoy@gmail.com>
2015-06-15 19:46:05 +1000

    Some improvements for expr

--- lecture-code/src/main/scala/test.sc
+++ lecture-code/src/main/scala/test.sc
@@ -1,4 +1,7 @@
+import week4.{Sum, Product, Number => MyNumber, exprs}
 object test {
   def error(msg: String) = throw new Error(msg)
+  exprs.show(Product(Sum(MyNumber(2), MyNumber(4)), Sum(MyNumber(6), MyNumber(8))))
+  exprs.eval(Product(Sum(MyNumber(2), MyNumber(4)), Sum(MyNumber(6), MyNumber(8))))
   error("ops")
 }
--- lecture-code/src/main/scala/week4/Expr.scala
+++ lecture-code/src/main/scala/week4/Expr.scala
@@ -6,14 +6,18 @@ case class Number(n: Int) extends Expr
 
 case class Sum(e1: Expr, e2: Expr) extends Expr
 
+case class Product(e1: Expr, e2: Expr) extends Expr
+
 object exprs {
   def eval(e: Expr): Int = e match {
     case Number(n) => n
     case Sum(e1, e2) => eval(e1) + eval(e2)
+    case Product(e1, e2) => eval(e1) * eval(e2)
   }
 
   def show(e: Expr): String = e match {
     case Number(n) => n.toString
-    case Sum(e1, e2) => show(e1) + " + " + show(e2)
+    case Sum(e1, e2) => "(" + show(e1) + " + " + show(e2) + ")"
+    case Product(e1, e2) => show(e1) + " * " + show(e2)
   }
 }
